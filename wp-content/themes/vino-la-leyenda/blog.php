27345ffa6f3141c1d03ecb35d6a8f87dc06b78fe
Darinel <darinelnietodiaz2021@gmail.com>
2021-11-24 23:27:49 -0500

    Estilos blog

--- wp-content/themes/vino-la-leyenda/blog.php
+++ wp-content/themes/vino-la-leyenda/blog.php
@@ -5,29 +5,30 @@
     get_header();
     $args = array("posts_per_page" => -1, "orderby" => "rand");
     $my_posts = get_posts($args);
+    $fondo = get_field('fondo_post', 'option');
 ?>
-<section class="bannerBlog">
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center">
-                <h2>Blog</h2>
+<section class="fondo-blog-content">
+    <div class="post-content" style="background-image:url(<?php echo $fondo ?>)">
+        <div class="bannerBlog">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h2>Blog</h2>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</section>
-<section>
-    <div class="post-content">
         <div class="container">
-            <div class="row">
+            <div class="row mt-5 mb-5">
                 <?php if($my_posts){ ?>
                     <?php foreach($my_posts as $post){ ?>
                         <div class="col-12 col-md-4 col-lg-3">
                             <a href="<?php echo get_permalink( $post->post_ID )?>">
-                                <div class="card">
+                                <div class="card card-post">
                                     <div class="content-image-post" style="background:url(<?php echo get_the_post_thumbnail_url($post->post_ID); ?>);"></div>
+                                    <h5 class="titulo-post-blog mt-2"><?php echo $post->post_title ?></h5>
                                     <div class="card-body">
-                                        <h4 class="tituloPostHome mb4"><?php echo $post->post_title ?></h4>
-                                        <p class="infoPostHome mt-3"><?php echo $post->descripcion_corta ?></p>
+                                        <p class="description-card-post"><?php echo $post->descripcion_corta ?></p>
                                     </div>
                                 </div>
                             </a>
